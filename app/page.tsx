d0741e93545da13f3cf3cf8ce691924f59e83d57
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-02-03 21:13:55 +0100

    Remove unused SVG assets and update the homepage layout
    Deleted obsolete SVG files from the `/public` directory to clean up unused assets. Updated the homepage to include an `Image` component displaying a new shirt image with improved styling. Added the shirt image to the `/public/images/shirts` directory.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
-    return <h1>Hello World</h1>;
+    return (
+        <div className="relative w-full max-w-4xl mx-auto bg-green-300 rounded-lg shadow-lg p-4">
+            <Image
+                src="/images/shirts/shirt_8.webp"
+                alt="Chelsea"
+                width={66}
+                height={66}
+            />
+        </div>
+    );
 }

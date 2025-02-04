7990b0ee94055e10d7740937be8958be1f0d96ea
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-02-04 20:47:10 +0100

    Enhance shirt card with styling and additional details
    Refact

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="relative w-full max-w-4xl mx-auto bg-green-300 rounded-lg shadow-lg p-4">
-            <Image
-                src="/images/shirts/shirt_8.webp"
-                alt="Chelsea"
-                width={66}
-                height={66}
-            />
+            <div className="card bg-base-100 w-26 shadow-sm">
+                <figure>
+                    <Image
+                        src="/images/shirts/shirt_8.webp"
+                        alt="Chelsea"
+                        width={66}
+                        height={66}
+                    />
+                </figure>
+                <div className="card-body p-0 items-center">
+                    <h2 className="card-title">Palmer</h2>
+                    <p>14</p>
+                </div>
+            </div>
         </div>
     );
 }
